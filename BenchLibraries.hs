b2c29db71edf74b5fb9effcfb41a03326dd9eaa2
Michael Burge <michael.burge@rentrak.com>
2013-09-17 18:52:42 -0700

    New benchmark for fromDigits

--- BenchLibraries.hs
+++ BenchLibraries.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.DeepSeq(NFData)
 import Criterion.Config
 import Criterion.Main
 import Common.Digits(foldDigits, fromDigits, toDigits)
@@ -7,13 +8,23 @@ import Types
 
 bigNumber :: Integer
 bigNumber = 1234567891011121314151617181920
+benchNParam :: NFData b => String -> (Integer -> a -> b) -> [Integer] -> a -> Benchmark
+benchNParam name f bs n = 
+    bgroup name $
+    map (\b -> bench (show b) $
+               nf (f b) n
+    ) bs
+baseBench name f = benchNParam name f [2,10] bigNumber
+
+foldDigitsTest b = foldDigits b (\xs x -> x:xs) []
+toAndFromTest b = fromDigits b . map (\x -> fromIntegral $ x + 1 `quot` b) . toDigits b
 
 main = Criterion.Main.defaultMain $
        [
-        bench "foldDigits 10" $ nf (foldDigits 10 (\xs x -> x:xs) []) bigNumber,
-        bench "foldDigits 2"  $ nf (foldDigits 2 (\xs x -> x:xs) []) bigNumber,
-        bench "toDigits 2"  $ nf (toDigits 2 ) bigNumber,
-        bench "toDigits 10" $ nf (toDigits 10) bigNumber,
-        bench "to-and-from digits 10" $ nf (fromDigits 10 . map (\x -> x + 1 `quot` 10) . toDigits 10) bigNumber,
-        bench "to-and-from digits 2" $ nf (fromDigits 2 . map (\x -> x + 1 `quot` 2) . toDigits 2) bigNumber
+        baseBench "foldDigits" foldDigitsTest,
+        baseBench "toDigits" toDigits,
+        baseBench "to-and-from-digits" toAndFromTest,
+        let digits = toDigits 3 bigNumber
+        in bench "fromDigits 3" $ nf (fromDigits 3) digits
+
        ]
