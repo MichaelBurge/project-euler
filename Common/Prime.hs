b49233328046b52b2b111245de00a03c7f1af78f
Michael Burge <michael.burge@rentrak.com>
2013-09-19 03:46:35 -0700

    Added faster prime-checker

--- Common/Prime.hs
+++ Common/Prime.hs
@@ -1,4 +1,4 @@
-module Common.Prime (modExp, primes, prime, factors) where
+module Common.Prime (modExp, isSprp, primes, prime, primeByPrp, primeByFactoring, primePower, factors) where
 import Prelude hiding (filter,
                        map,
                        null
@@ -11,12 +11,46 @@ import Data.Set
 sum' = fold (+) 0
 primes = allPrimes
 
+prime :: Integer -> Bool
+prime = primeByPrp
+
 modExp :: Integer -> Integer -> Integer -> Integer
 modExp _ _ 0 = 1
 modExp mod base 1 = base `rem` mod
 modExp mod base exponent = flip rem mod $
     let next = modExp mod (base * base `rem` mod) (exponent `div` 2)
     in next * (if odd exponent then base else 1)
+
+primePower n p =
+  let f x i = let (q, r) = x `quotRem` p
+              in if r == 0
+                 then f q (i+1)
+                 else i
+  in f n 0
+
+oddPart n = if odd n then n else oddPart (n `div` 2)
+
+-- See Section 2.3 of http://primes.utm.edu/prove/merged.html
+isPrp n a = 1 == (modExp n a (n-1))
+isSprp n a =
+    let s  = primePower (n-1) 2
+        d = oddPart $ n-1
+    in (1 == modExp n a d) ||
+       any (\r -> (n-1) == modExp n (a^d) (2^r)) [0..s]
+
+-- See the table in Section 2.3 of http://primes.utm.edu/prove/merged.html
+primeByPrp n = let
+  checkPrp a = isSprp n a
+  in case () of
+       _ | n < 4 -> n == 2 || n == 3
+         | smallPrimeDivides n -> False
+         | n < 1373653 -> checkPrp 2 && checkPrp 3
+         | n < 25326001 -> checkPrp 2 && checkPrp 3 && checkPrp 5
+         | n < 118670087467 -> checkPrp 2 && checkPrp 3 && checkPrp 5 && checkPrp 7 && n /= 3215031751 
+         | n < 2152302898747 -> all checkPrp [2,3,5,7,11]
+         | n < 3474749660383 -> all checkPrp [2,3,5,7,11,13]
+         | n < 341550071728321 -> all checkPrp [2,3,5,7,11,13,17]
+         | otherwise -> error $ "Add more prp checks to primeByPrp. Received: " ++ show n
 
 isFactor n k = 0 == (rem n k)
 possibleFactors n = List.takeWhile (\x -> x*x <= n) [1..]
@@ -30,9 +64,9 @@ factors n =
 
 smallPrimeDivides n = any (\p -> n /= p && n `rem` p == 0) smallPrimes
 
-prime n | n < 2 = False
-prime n | smallPrimeDivides n = False
-prime n = null $ (factors n) \\ (fromList [1, n])
+primeByFactoring n | n < 2 = False
+primeByFactoring n | smallPrimeDivides n = False
+primeByFactoring n = null $ (factors n) \\ (fromList [1, n])
 
 allPrimes = (:) 2 $ 
          Prelude.filter (\x -> all (\y -> (x `rem` y) /= 0) $ 
--- Common/Tests/Prime.hs
+++ Common/Tests/Prime.hs
@@ -1,6 +1,6 @@
 module Common.Tests.Prime (primesTests) where
 
-import Common.Prime (modExp, prime, primes)
+import Common.Prime (modExp, prime, primeByPrp, primeByFactoring, primePower, primes)
 import Test.Framework
 import Test.Framework.Providers.HUnit (testCase)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
@@ -11,12 +11,24 @@ import Test.QuickCheck
 test_primes = assert $ takeWhile (<20) primes == [2,3,5,7,11,13,17,19]
 test_prime = assert $ all prime [19, 211, 877, 27644437]
 
-prop_fermat_little_theorem (Positive p) (Positive a) =
-    (prime p) ==> ((modExp p a p) == (a `rem` p))
+prop_fermat_little_theorem (Positive n) (Positive a) =
+    (n > 2) ==> 
+    forAll (elements (takeWhile (<n) primes)) $ \p ->
+        (modExp p a p) == (a `rem` p)
+
+prop_primeByPrp_iff_primeByFactoring (Positive n) =
+    primeByPrp n == primeByFactoring n
+
+test_primePower =
+    assert $
+    map (primePower (5^4 * 7^3 * 2^7)) [2,3,5,7] ==
+    [7, 0, 4, 3]
 
 primesTests =
   testGroup "Primes" [
       testCase "primes" test_primes,
       testCase "prime" test_prime,
-      testProperty "Fermat's Little Theorem" prop_fermat_little_theorem
+      testProperty "Fermat's Little Theorem" prop_fermat_little_theorem,
+      testProperty "PRP primality test" prop_primeByPrp_iff_primeByFactoring,
+      testCase "primePower" test_primePower
   ]
